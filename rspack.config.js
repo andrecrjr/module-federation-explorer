a0fe402b14727f9ed6fa2c182a55eaa7e97c1ce2
André Carlos Jr <7965520+andrecrjr@users.noreply.github.com>
2025-03-25 23:49:53 -0300

    - cleaning things

--- rspack.config.js
+++ rspack.config.js
@@ -1,27 +1,22 @@
-/*---------------------------------------------------------------------------------------------
- *  Copyright (c) Microsoft Corporation. All rights reserved.
- *  Licensed under the MIT License. See License.txt in the project root for license information.
- *--------------------------------------------------------------------------------------------*/
-
 'use strict';
 
 const path = require('path');
 
 /**@type {import('@rspack/cli').Configuration}*/
 const config = {
-    target: 'node', // Rspack also supports Node.js context
+    target: 'node',
 
-    entry: './src/index.ts', // the entry point of this extension
-    output: { // the bundle is stored in the 'dist' folder
+    entry: './src/index.ts', 
+    output: { 
         path: path.resolve(__dirname, 'dist'),
         filename: 'extension.js',
         libraryTarget: "commonjs2",
     },
-    devtool: 'source-map', // Rspack supports source maps
+    devtool: 'source-map',
     externals: {
-        vscode: "commonjs vscode", // Exclude vscode from the bundle
+        vscode: "commonjs vscode",
     },
-    resolve: { // support reading TypeScript and JavaScript files
+    resolve: {
         extensions: ['.ts', '.js']
     },
     module: {
@@ -29,7 +24,7 @@ const config = {
             test: /\.ts$/,
             exclude: /node_modules/,
             use: [{
-                loader: 'ts-loader', // Rspack supports ts-loader
+                loader: 'ts-loader',
             }]
         }]
     },
